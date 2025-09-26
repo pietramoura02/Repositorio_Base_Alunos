8b197b03492ed5f7b048f9d5fda71cb7b46572bc
pietramoura02 <piehh9679@gmail.com>
2025-09-26 15:21:54 -0300

    Delete 02manipulacao_arq.py

--- 02manipulacao_arq.py
+++
@@ -1,7 +0,0 @@
-# ler um arquivo 
-
-arquivo = open('pessoa.txt', 'r') # a leitura está sendo feita na maemoria 
-conteudo = arquivo.read() # eu armazeno a leitura em uma variavel 
-print(conteudo) # peço para imprimir a leitura 
-arquivo.close() # sempre que utilizamos a função open () precisamos finalizar 
-# fechando o arquivo 
