82bc15eca9c672fe31c74319e4432c5289d426a0
pietramoura02 <piehh9679@gmail.com>
2025-09-26 15:23:28 -0300

    Delete 04manipulacao_arq.py

--- 04manipulacao_arq.py
+++
@@ -1,7 +0,0 @@
-# adicionar uma nova frase no meu arquivo 
-
-# caso eu queira adicionar uma nova frase no arquivo já criado 
-# utilizamos o modo 'a' para não subescrever no conteudo existente 
-
-with open('mensagem.txt','a', encoding='utf-8') as arquivo: 
-    arquivo.write('\nAprendendo a manipular arquivos. ')
